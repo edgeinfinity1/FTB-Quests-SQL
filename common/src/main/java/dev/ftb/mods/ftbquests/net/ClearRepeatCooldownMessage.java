2c434e0a3f67a6a9b73a7325313e2211e77acd12
Des Herriott <des.herriott@gmail.com>
2026-01-23 10:32:00 +0000

    fix: post cherry-pick fixup

--- common/src/main/java/dev/ftb/mods/ftbquests/net/ClearRepeatCooldownMessage.java
+++ common/src/main/java/dev/ftb/mods/ftbquests/net/ClearRepeatCooldownMessage.java
@@ -1,38 +1,44 @@
 package dev.ftb.mods.ftbquests.net;
 
 import dev.architectury.networking.NetworkManager;
-import dev.ftb.mods.ftblibrary.util.NetworkHelper;
-import dev.ftb.mods.ftbquests.api.FTBQuestsAPI;
+import dev.architectury.networking.simple.BaseS2CMessage;
+import dev.architectury.networking.simple.MessageType;
 import dev.ftb.mods.ftbquests.client.ClientQuestFile;
 import dev.ftb.mods.ftbquests.quest.Quest;
-import net.minecraft.network.RegistryFriendlyByteBuf;
-import net.minecraft.network.codec.ByteBufCodecs;
-import net.minecraft.network.codec.StreamCodec;
-import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.MinecraftServer;
 
-public record ClearRepeatCooldownMessage(long id) implements CustomPacketPayload {
-	public static final Type<ClearRepeatCooldownMessage> TYPE = new Type<>(FTBQuestsAPI.rl("clear_repeat_cooldown"));
+public class ClearRepeatCooldownMessage extends BaseS2CMessage {
+	private final long id;
 
-	public static final StreamCodec<RegistryFriendlyByteBuf, ClearRepeatCooldownMessage> STREAM_CODEC = StreamCodec.composite(
-			ByteBufCodecs.VAR_LONG, ClearRepeatCooldownMessage::id,
-			ClearRepeatCooldownMessage::new
-	);
+    public ClearRepeatCooldownMessage(long id) {
+        this.id = id;
+    }
 
-	@Override
-	public Type<ClearRepeatCooldownMessage> type() {
-		return TYPE;
-	}
-
-	public static void handle(ClearRepeatCooldownMessage message, NetworkManager.PacketContext context) {
-		context.queue(() -> {
-			if (ClientQuestFile.exists() && ClientQuestFile.INSTANCE.getBase(message.id) instanceof Quest quest) {
-				ClientQuestFile.INSTANCE.selfTeamData.clearRepeatCooldown(quest);
-			}
-		});
+	public ClearRepeatCooldownMessage(FriendlyByteBuf buf) {
+		this.id = buf.readLong();
 	}
 
 	public static void sendToAll(MinecraftServer server, Quest quest) {
-		NetworkHelper.sendToAll(server, new ClearRepeatCooldownMessage(quest.getId()));
+		new ClearRepeatCooldownMessage(quest.id).sendToAll(server);
+	}
+
+	@Override
+	public MessageType getType() {
+		return FTBQuestsNetHandler.CLEAR_REPEAT_COOLDOWN;
+	}
+
+	@Override
+	public void write(FriendlyByteBuf buf) {
+		buf.writeLong(id);
+	}
+
+	@Override
+	public void handle(NetworkManager.PacketContext context) {
+		context.queue(() -> {
+			if (ClientQuestFile.exists() && ClientQuestFile.INSTANCE.getBase(id) instanceof Quest quest) {
+				ClientQuestFile.INSTANCE.selfTeamData.clearRepeatCooldown(quest);
+			}
+		});
 	}
 }
